30113fdf0de36f3d935eb4f43a7f048757916786
jm672 <jm672@njit.edu>
2021-03-16 20:16:52 -0400

    Update to pref buttons

--- preferences.php
+++ preferences.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['user_id'])) { // Checks if the user is currently logged in.
             $stmt->execute([$_SESSION["user_id"],]);
             
             foreach ($_POST as $param_name => $param_val) {
+                $param_name = str_replace("", " ", $param_name);
                 if ($param_name == 'bakery' || $param_name == 'bar' || $param_name == 'cafe' || $param_name == 'convenience_store' || $param_name == 'liquor_store' || $param_name == 'meal_delivery' || $param_name == 'meal_takeaway' || $param_name == 'restaurant' || $param_name == 'supermarket') {
                     $stmt = $pdo->prepare("INSERT INTO food (uid, preference) VALUES (?, ?)");
                     $stmt->execute([$_SESSION["user_id"], $param_name,]);
@@ -49,22 +50,21 @@ if (isset($_SESSION['user_id'])) { // Checks if the user is currently logged in.
                         </h1>
 
                         <div class="choices">
-                            
-                            <input type="checkbox" placeholder="Amusement Park" name="amusement park"> <label for="amusement park">Amusement Parks</label>
-                            <input type="checkbox" placeholder="Aquarium" name="aquarium"> <label for="aquarium">Aquariums</label>
-                            <input type="checkbox" placeholder="Art Gallery" name="art gallery"> <label for="art gallery">Art Galleries</label>
-                            <input type="checkbox" placeholder="Bowling Alley" name="bowling alley"> <label for="bowling alley">Bowling Alleys</label>
-                            <input type="checkbox" placeholder="Casino" name="casino"> <label for="casinos">Casinos</label>
-                            <input type="checkbox" placeholder="Gym" name="gym"> <label for="gym">Gyms</label>
-                            <input type="checkbox" placeholder="Library" name="library"> <label for="library">Libraries</label>
-                            <input type="checkbox" placeholder="Mall" name="mall"> <label for="mall">Malls</label>
-                            <input type="checkbox" placeholder="Movie Rental" name="movie rental"> <label for="movie rental">Movie Rentals</label>
-                            <input type="checkbox" placeholder="Movie Theater" name="movie theater"> <label for="movie theater">Movie Theaters</label>
-                            <input type="checkbox" placeholder="Night Club" name="night club"> <label for="night club">Night Clubs</label>
-                            <input type="checkbox" placeholder="Park" name="park"> <label for="park">Parks</label>
-                            <input type="checkbox" placeholder="Stadium" name="stadium"> <label for="stadium">Stadiums</label>
-                            <input type="checkbox" placeholder="Tourist Attraction" name="tourist attraction"> <label for="tourist attraction">Tourist Attractions</label>
-                            <input type="checkbox" placeholder="Zoo" name="zoo"> <label for="zoo">Zoos</label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Amusement Park" name="amusement park"><span class="checkbox-span"> Amusement Parks</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Aquarium" name="aquarium"><span class="checkbox-span"> Aquariums</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Art Gallery" name="art gallery"><span class="checkbox-span"> Art Galleries</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Bowling Alley" name="bowling alley"><span class="checkbox-span"> Bowling Alleys</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Casino" name="casino"><span class="checkbox-span"> Casinos</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Gym" name="gym"><span class="checkbox-span"> Gyms</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Library" name="library"><span class="checkbox-span"> Libraries</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Mall" name="mall"><span class="checkbox-span"> Malls</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Movie Rental" name="movie rental"><span class="checkbox-span"> Movie Rentals</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Movie Theater" name="movie theater"><span class="checkbox-span"> Movie Theaters</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Night Club" name="night club"><span class="checkbox-span"> Night Clubs</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Park" name="park"><span class="checkbox-span"> Parks</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Stadium" name="stadium"><span class="checkbox-span"> Stadiums</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Tourist Attraction" name="tourist attraction"><span class="checkbox-span"> Tourist Attractions</span></label>
+                            <label class="checkbox"><input type="checkbox" placeholder="Zoo" name="zoo"><span class="checkbox-span"> Zoos</span></label>
                             
                         </div>
 
@@ -77,15 +77,15 @@ if (isset($_SESSION['user_id'])) { // Checks if the user is currently logged in.
                         </h1>
 
                         <div class="choices">
-                        <input type="checkbox" placeholder="Bakery" name="bakery"> <label for="bakery">Bakeries</label>
-                        <input type="checkbox" placeholder="Bar" name="bar"> <label for="bar">Bars</label>
-                        <input type="checkbox" placeholder="Cafe" name="cafe"> <label for="cafe">Cafes</label>
-                        <input type="checkbox" placeholder="Convenience Store" name="convenience store"> <label for="convenience_store">Convenience Stores</label>
-                        <input type="checkbox" placeholder="Liquor Store" name="liquor store"> <label for="liquor store">Liquor Stores</label>
-                        <input type="checkbox" placeholder="Meal Deliveryk" name="meal delivery"><label for="meal delivery">Meal Deliveries</label>
-                        <input type="checkbox" placeholder="Meal Takeaway" name="meal takeaway"><label for="meal takeaway">Meal Takeaways</label>
-                        <input type="checkbox" placeholder="Restaurant" name="restaurant"> <label for="restaurant">Restaurants</label>
-                        <input type="checkbox" placeholder="Supermarket" name="supermarket"> <label for="supermarket">Supermarkets</label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Bakery" name="bakery"><span class="checkbox-span"> Bakeries</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Bar" name="bar"><span class="checkbox-span"> Bars</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Cafe" name="cafe"><span class="checkbox-span"> Cafes</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Convenience Store" name="convenience store"><span class="checkbox-span"> Convenience Stores</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Liquor Store" name="liquor store"><span class="checkbox-span"> Liquor Stores</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Meal Deliveryk" name="meal delivery">Meal Deliveries</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Meal Takeaway" name="meal takeaway">Meal Takeaways</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Restaurant" name="restaurant"><span class="checkbox-span"> Restaurants</span></label>
+                        <label class="checkbox"><input type="checkbox" placeholder="Supermarket" name="supermarket"><span class="checkbox-span"> Supermarkets</span></label>
                         
                       </div>
 
